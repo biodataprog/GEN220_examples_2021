a9de5638da1d0c98168e7e257aaea56bc408fe0a
Jason Stajich <jasonstajich.phd@gmail.com>
2021-10-15 14:45:39 -0700

    more examples

---
+++ Python_examples/math.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+sum=0
+
+sum+=1
+sum+=1
+sum+=1
+sum+=1
+sum+=1
+
+print("sum is:", sum)
---
+++ Python_examples/strings.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+str = "Jumping cow over the moon"
+
+print(str.find("cow"))
+              
+msg="I am a golden god!"
+               
